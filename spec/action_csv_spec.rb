19959cfd165529025d677d411d539d6727fa3e1b
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-04-15 01:25:09 +0900

    remove test

--- spec/action_csv_spec.rb
+++ spec/action_csv_spec.rb
@@ -4,14 +4,4 @@ RSpec.describe ActionCsv do
   it "has a version number" do
     expect(ActionCsv::VERSION).not_to be nil
   end
-
-  it "has config dir" do
-    a1 = Dir.exist? "config"
-    expect(a1).to eq(true)
-  end
-
-  it "has db dir" do
-    a1 = Dir.exist? "db"
-    expect(a1).to eq(true)
-  end
 end
